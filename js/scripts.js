f7bae9dd63f01cd775720e17771d08dfccdb5b72
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 12:52:51 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -80,7 +80,25 @@ $(".input").keyup(function () {
       }
       jo.hide();
 
-
+			jo.filter(function (i, v) {
+		           var $t = $(this);
+		           for (var d = 0; d < data.length; ++d) {
+		               if ($t.text().toUpperCase().indexOf(data[d]) > -1) {
+		                   return true;
+		               }
+		           }
+		           return false;
+		       })
+		       .show();
+		   }).focus(function () {
+		       this.value = "";
+		       $(this).css({
+		           "color": "black"
+		       });
+		       $(this).unbind('focus');
+		   }).css({
+		       "color": "#C0C0C0"
+		   });
 
 var navigate = (function() {
 	$('.dd').toggle();
